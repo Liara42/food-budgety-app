7991dbd1a8ab338e95b6abcbc5a086768758ff76
Liara42 <caroline1481@gmail.com>
2020-08-02 22:45:49 +0200

    Fixed bugs

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -3,38 +3,40 @@ import { elements } from './base';
 export const getInput = () => elements.searchInput.value;
 
 export const clearInput = () => {
-    elements.searchInput.value = '';
+  elements.searchInput.value = '';
 };
 
 export const clearResults = () => {
-    elements.searchResList.innerHTML = '';
-    elements.searchResPages.innerHTML = '';
+  elements.searchResList.innerHTML = '';
+  elements.searchResPages.innerHTML = '';
 };
 
-export const highlightSelected = id => {
-    const resultsArr = Array.from(document.querySelectorAll('.results__link'));
-    resultsArr.forEach(el => {
-        el.classList.remove('results__link--active');
-    })
+export const highlightSelected = (id) => {
+  const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+  resultsArr.forEach((el) => {
+    el.classList.remove('results__link--active');
+  });
 
-    document.querySelector(`.results__link[href="#${id}"]`).classList.add('results__link--active')
-}
+  document
+    .querySelector(`.results__link[href="#${id}"]`)
+    .classList.add('results__link--active');
+};
 
-const limitRecipeTitle = (title, limit = 17) => {
-    const newTitle = [];
-    if (title.length > limit) {
-        title.split(' ').reduce((acc, cur) => {
-            if (acc + cur.length <= limit) {
-                newTitle.push(cur);
-            }
-        }, 0);
-        return `${newTitle.join(' ')} ...`;
-    }
-    return title;
-}
+export const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(' ').reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+    }, 0);
+    return `${newTitle.join(' ')} ...`;
+  }
+  return title;
+};
 
-const renderRecipe = recipe => {
-    const markup = `
+const renderRecipe = (recipe) => {
+  const markup = `
     <li>
         <a class="results__link" href = "#${recipe.recipe_id}" >
             <figure class="results__fig">
@@ -48,48 +50,50 @@ const renderRecipe = recipe => {
     </li>
     `;
 
-    elements.searchResList.insertAdjacentHTML('beforeend', markup);
-}
+  elements.searchResList.insertAdjacentHTML('beforeend', markup);
+};
 
 const createButton = (page, type) => `
-<button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+<button class="btn-inline results__btn--${type}" data-goto=${
+  type === 'prev' ? page - 1 : page + 1
+}>
 <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
     <svg class="search__icon">
-        <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        <use href="img/icons.svg#icon-triangle-${
+          type === 'prev' ? 'left' : 'right'
+        }"></use>
     </svg>
 </button>
 `;
 
 const renderButtons = (page, numResults, resPerPage) => {
-    const pages = Math.ceil(numResults / resPerPage);
+  const pages = Math.ceil(numResults / resPerPage);
 
-    let button;
+  let button;
 
-    if (page === 1 && pages > 1) {
-        //Only one button to go to next page
-        button = createButton(page, 'next');
-    } else if (page < pages) {
-        //Both buttons
-        button = `
+  if (page === 1 && pages > 1) {
+    //Only one button to go to next page
+    button = createButton(page, 'next');
+  } else if (page < pages) {
+    //Both buttons
+    button = `
             ${createButton(page, 'prev')}
             ${createButton(page, 'next')}`;
-    } else if (page === pages && pages > 1) {
-        //Only one button to go to previous page
-        button = createButton(page, 'prev');
-    }
+  } else if (page === pages && pages > 1) {
+    //Only one button to go to previous page
+    button = createButton(page, 'prev');
+  }
 
-    elements.searchResPages.insertAdjacentHTML('afterbegin', button);
-}
+  elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+};
 
 export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+  //render results of current page
+  const start = (page - 1) * resPerPage;
+  const end = page * resPerPage;
 
-    //render results of current page
-    const start = (page - 1) * resPerPage;
-    const end = page * resPerPage;
+  recipes.slice(start, end).forEach(renderRecipe);
 
-    recipes.slice(start, end).forEach(renderRecipe);
-
-    //render pagination numbers
-    renderButtons(page, recipe.length, resPerPage);
-
+  //render pagination numbers
+  renderButtons(page, recipe.length, resPerPage);
 };
